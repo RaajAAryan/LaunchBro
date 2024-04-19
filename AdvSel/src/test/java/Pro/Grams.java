3ae25c69a20939ccbfb7f0a614863c3cd421b014
RaajAAryan <rajkiranrd27@gmail.com>
2024-04-19 18:00:56 +0530

    excute

---
+++ AdvSel/src/test/java/Pro/Grams.java
@@ -0,0 +1,18 @@
+package Pro;
+
+import org.testng.annotations.Test;
+
+public class Grams {
+	@Test
+	public void TestRepo1() {
+		System.out.println("Execute my program");
+	}
+	@Test
+	public void TestRepo2() {
+		System.out.println("dont Execute");
+	}
+	@Test
+	public void TestRepo3() {
+		System.out.println("Please Execute my program");
+	}
+}
