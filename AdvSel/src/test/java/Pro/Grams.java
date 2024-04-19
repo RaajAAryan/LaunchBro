d185ecdc2cb140bebeca6a4ba50c4afcaf4f096d
RaajAAryan <rajkiranrd27@gmail.com>
2024-04-19 18:13:44 +0530

    Second commit

--- AdvSel/src/test/java/Pro/Grams.java
+++ AdvSel/src/test/java/Pro/Grams.java
@@ -1,5 +1,11 @@
 package Pro;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Grams {
@@ -14,5 +20,24 @@ public class Grams {
 	@Test
 	public void TestRepo3() {
 		System.out.println("Please Execute my program");
+	}
+	public void LaunChhrome() {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.get("https://www.amazon.in");
+	}
+	public void LaunEddge() {
+		WebDriver driver=new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.get("https://www.flipkart.in");
+	}
+	
+	public void LaunFiooreFox() {
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.get("https://www.facebook.com");
 	}
 }
