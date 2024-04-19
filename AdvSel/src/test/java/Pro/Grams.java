512d60d8259ab4cf82263c6b4829f8e655533980
RaajAAryan <rajkiranrd27@gmail.com>
2024-04-19 23:52:57 +0530

    second commit

--- AdvSel/src/test/java/Pro/Grams.java
+++ AdvSel/src/test/java/Pro/Grams.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import com.mysql.cj.x.protobuf.MysqlxConnection.Close;
+
 public class Grams {
 	@Test
 	public void TestRepo1() {
@@ -21,19 +23,21 @@ public class Grams {
 	public void TestRepo3() {
 		System.out.println("Please Execute my program");
 	}
+	@Test
 	public void LaunChhrome() {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 		driver.get("https://www.amazon.in");
 	}
+	@Test
 	public void LaunEddge() {
 		WebDriver driver=new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 		driver.get("https://www.flipkart.in");
 	}
-	
+	@Test
 	public void LaunFiooreFox() {
 		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
